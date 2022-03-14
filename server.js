2c357a8e54e194de0ac367515b9c4d7bdc8d1d09
Jane Tiglao <jane.b.tiglao@gmail.com>
2022-03-14 03:54:58 -0700

    updated, routes added

--- server.js
+++ server.js
@@ -12,12 +12,13 @@ const PORT = process.env.PORT || 3000;
 // Set up express to handle data parsing
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(express.static(--dirname));
+app.use(express.static("public"));
 
-// Routes file
-require("./routes/routes")(app);
+// Routes 
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 // Set up listener
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
+app.listen(PORT, () => {
+    console.log("App is listening on PORT: " + PORT);
 });
