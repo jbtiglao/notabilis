4dbdd1c35029bd8b70a2192d480b42edfab11870
Jane Tiglao <jane.b.tiglao@gmail.com>
2022-03-14 03:57:00 -0700

    initial commit, api & html routes.js files created

---
+++ routes/apiRoutes.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+module.exports = function(app) {
+    let notes = reuire("../db/db.json")
+
+    // Set up the /api/notes get route
+
+    // Set up the /api/notes post route
+
+    // Retrieves a note with specific id
+
+    // Deletes a note with a specific id
+
+
+    
+}
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+module.exports = function(app) {
+
+
+    // Display notes.html when /notes is accessed
+
+
+    // Updates json file when a note is added or deleted
+}
